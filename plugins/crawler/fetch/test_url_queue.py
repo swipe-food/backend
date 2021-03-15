bb673db2265d65e7802092e338a0eb9ac4fd8787
Jonas Scholl <jonas@scholl.tech>
2021-03-15 18:41:17 +0100

    URLQueue implementation

--- plugins/crawler/fetch/test_url_queue.py
+++ plugins/crawler/fetch/test_url_queue.py
@@ -1,3 +1,5 @@
+import pytest
+
 from plugins.crawler.fetch.url_queue import URLQueue
 
 
@@ -7,66 +9,66 @@ class TestUrlQueue:
         'https://xkcd.com/',
     ]
 
-    @classmethod
-    def url_queue(cls) -> URLQueue:
+    @staticmethod
+    @pytest.fixture
+    def url_queue() -> URLQueue:
         return URLQueue()
 
-    @classmethod
-    def test_create_empty_url_queue(cls, url_queue: URLQueue):
+    def test_create_empty_url_queue(self, url_queue: URLQueue):
         assert url_queue.urls == list()
 
-    @classmethod
-    def test_create_filled_url_queue(cls):
-        url_queue = URLQueue(cls.test_urls)
-        assert url_queue.urls == cls.test_urls
+    def test_create_filled_url_queue(self):
+        url_queue = URLQueue(self.test_urls)
+        assert url_queue.urls == self.test_urls
 
-    @classmethod
-    def test_fill_url_queue(cls, url_queue: URLQueue):
-        url_queue.add(cls.test_urls)
-        assert url_queue.urls == cls.test_urls
+    def test_fill_url_queue(self, url_queue: URLQueue):
+        url_queue.add(self.test_urls)
+        assert url_queue.urls == self.test_urls
 
-    @classmethod
-    def test_add_url_to_queue(cls, url_queue: URLQueue):
+    def test_add_url_to_queue(self, url_queue: URLQueue):
         new_url = 'https://code-specialist.com'
         previous_length = len(url_queue.urls)
         url_queue.add(new_url)
         assert len(url_queue.urls) == previous_length + 1
         assert 'https://code-specialist.com' in url_queue.urls
 
-    @classmethod
-    def test_url_queue_builtin_add(cls, url_queue: URLQueue):
+    def test_url_queue_builtin_add(self, url_queue: URLQueue):
         new_url = 'https://code-specialist.com'
         previous_length = len(url_queue.urls)
         url_queue += new_url
         assert len(url_queue.urls) == previous_length + 1
         assert 'https://code-specialist.com' in url_queue.urls
 
-    @classmethod
-    def test_url_queue_clear(cls, url_queue: URLQueue):
-        url_queue.add(cls.test_urls)
+    def test_url_queue_add_fail(self, url_queue: URLQueue):
+        with pytest.raises(ValueError):
+            url_queue.add(42)
+
+    def test_url_queue_clear(self, url_queue: URLQueue):
+        url_queue.add(self.test_urls)
         url_queue.clear()
         assert url_queue.urls == list()
 
-    @classmethod
-    def test_url_queue_len(cls, url_queue: URLQueue):
+    def test_url_queue_len(self, url_queue: URLQueue):
         assert len(url_queue) == 0
-        url_queue.add(cls.test_urls)
-        assert len(url_queue) == len(cls.test_urls)
+        url_queue.add(self.test_urls)
+        assert len(url_queue) == len(self.test_urls)
 
-    @classmethod
-    def test_url_queue_is_empty_true(cls, url_queue: URLQueue):
+    def test_url_queue_is_empty_true(self, url_queue: URLQueue):
         assert url_queue.is_empty()
 
-    @classmethod
-    def test_url_queue_is_empty_false(cls, url_queue: URLQueue):
-        url_queue.add(cls.test_urls)
+    def test_url_queue_is_empty_false(self, url_queue: URLQueue):
+        url_queue.add(self.test_urls)
         assert not url_queue.is_empty()
 
-    @classmethod
-    def test_url_queue_bool_true(cls, url_queue: URLQueue):
-        url_queue.add(cls.test_urls)
+    def test_url_queue_bool_true(self, url_queue: URLQueue):
+        url_queue.add(self.test_urls)
         assert url_queue
 
-    @classmethod
-    def test_url_queue_bool_false(cls, url_queue: URLQueue):
+    def test_url_queue_bool_false(self, url_queue: URLQueue):
         assert not url_queue
+
+    def test_process_url_queue(self, url_queue: URLQueue):
+        url_queue.add(self.test_urls)
+        for url in url_queue:
+            assert url in self.test_urls
+        assert url_queue.is_empty()
--- plugins/crawler/fetch/url_queue.py
+++ plugins/crawler/fetch/url_queue.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import List
 
 
@@ -5,3 +7,39 @@ class URLQueue:
 
     def __init__(self, urls: List[str] = None):
         self.urls = urls if urls else list()
+
+    def add(self, value: str or List[str]):
+        if isinstance(value, str):
+            self.urls.append(value)
+        elif isinstance(value, list):
+            for item in value:
+                self.add(item)
+        else:
+            raise ValueError(f'unsopported type {type(value)} for {self}')
+
+    def clear(self):
+        self.urls.clear()
+
+    def is_empty(self):
+        return len(self) == 0
+
+    def __add__(self, other) -> URLQueue:
+        self.add(other)
+        return self
+
+    def __len__(self) -> int:
+        return len(self.urls)
+
+    def __bool__(self):
+        return not self.is_empty()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.is_empty():
+            return self.urls.pop(0)
+        raise StopIteration
+
+    def __repr__(self) -> str:
+        return f'URLQueue({len(self)} urls)'
