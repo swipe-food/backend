49ae5432bf0403863a5a856a3e48419dece725e8
Dennis Geiselmann <dennis.geiselmann@outlook.de>
2021-03-27 20:54:02 +0100

    add basic crud functions to postgres database

--- infrastructure/storage/sql/postgres.py
+++ infrastructure/storage/sql/postgres.py
@@ -5,6 +5,7 @@ from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
+from common.exceptions import MissingArgumentException
 from infrastructure.config import DatabaseConfig
 from infrastructure.log import Logger
 from infrastructure.storage.sql.model import Base
@@ -42,12 +43,35 @@ class PostgresDatabase:
         self._logger.info(f'connected to database', engine=self._engine.__str__())
 
     def create_tables(self):
+        Base.metadata.drop_all(bind=self._engine)
         Base.metadata.create_all(bind=self._engine)
         self._logger.info('created tables')
 
-    def create(self, *items: Base):
+    def add(self, *items: Base):
+        """Adds one or multiple items to the database"""
         for item in items:
             self._session.add(item)
+        self._session.commit()
+
+    def update(self, table: Base, filters: tuple, data: dict):
+        """Updates a row in the database
+        :param table: class of the table that should get altered
+        :param filters: list of comparisons, which are the filters for the query ([User.id == user_id] for instance)
+        :param data: dict with the column names as keys and the values as the new data, doesn't have to contain all columns
+        """
+        if not filters or not data:
+            raise MissingArgumentException(f'either param filters ({filters}) or param data ({data}) is empty!')
+        self._session.query(table).filter(*filters).update(data, synchronize_session='evaluate')
+        self._session.commit()
+
+    def delete(self, table: Base, filters: tuple):
+        """Deletes rows from the database
+        :param table: class of the table that should get altered
+        :param filters: list of comparisons, which are the filters for the query ([User.id == user_id] for instance)
+        """
+        if not filters:
+            raise MissingArgumentException(f'param filters ({filters}) is empty!')
+        self._session.query(table).filter(*filters).delete(synchronize_session='fetch')
         self._session.commit()
 
     @property
